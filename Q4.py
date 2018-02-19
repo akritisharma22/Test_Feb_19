f0e3ef3cec6aad33cf8f1c62b308ca5ecad7bc17
akritisharma22 <akritis801@gmail.com>
2018-02-19 11:06:29 +0545

    printing nationality

---
+++ Q4.py
@@ -0,0 +1,13 @@
+'''Q4.Define
+a class named Nepali which has a static method called
+ printNationality.Which when invoked prints "Nepalese"
+'''
+
+class Nepali:
+
+    @staticmethod
+    def printNationality():
+        print("Nepalese")
+
+
+Nepali.printNationality()
